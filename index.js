3c78bd3fa11cf8dbdac9f3675373fa2666e7d7e4
kvandana451 <kvandana451@gmail.com>
2022-02-28 20:38:18 +0530

    Completed random coffee image generator

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
+const imgContainer = document.getElementById("image-container");
+
 fetch("https://coffee.alexflipnote.dev/random.json")
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    displayImage(data);
+  });
+
+function createElements(src) {
+  const img = document.createElement("img");
+  img.setAttribute("src", src);
+  img.style.width = "600px";
+  img.style.height = "500px";
+  imgContainer.appendChild(img);
+}
+
+function displayImage(data) {
+  const image = data.file;
+  createElements(image);
+}
